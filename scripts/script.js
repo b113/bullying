f9b000032fd0e07678861020eeafd9270e433ff1
Sergey Bakhar <b113.bahar@gmail.com>
2019-01-05 14:37:12 +0200

    finish youtube section

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
-	$(function(){
-		$('#menu').slicknav({
-            label:'Меню',
-            prependTo: '.header'
-        });
-	});
+$(function () {
+    $('#menu').slicknav({
+        label: 'Меню',
+        prependTo: '.header'
+    });
+
+
+    $('.youtube__btn').hover(function () {
+        $('.youtube__fill').css('opacity', '0');
+    }, function () {
+        $('.youtube__fill').css('opacity', '0.25');
+    });
+});
+
+
