af74c828f936aedefff837a7f361a7711587370c
Sergey Bakhar <b113.bahar@gmail.com>
2019-01-06 13:47:36 +0200

    correct scss files

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 $(function () {
-    $('#menu').slicknav({
-        label: 'Меню',
-        prependTo: '.header'
-    });
+  $('#menu').slicknav({
+    label: 'Меню',
+    prependTo: '.header'
+  });
 
 
-    $('.youtube__btn').hover(function () {
-        $('.youtube__fill').css('opacity', '0');
-    }, function () {
-        $('.youtube__fill').css('opacity', '0.25');
-    });
+  $('.youtube__btn').hover(function () {
+    $('.youtube__fill').css('opacity', '0');
+  }, function () {
+    $('.youtube__fill').css('opacity', '0.25');
+  });
 });
 
 
